84e7790a64442f9b171330cfd5a1c0d3989d4b2a
Shaqeal Jinnah <j.shaqeal@gmail.com>
2023-06-08 22:03:47 +1000

    CSS Tweaks

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // http://www.omdbapi.com/?apikey=b0099fba&s=fast
 
+const moviesContainer = document.querySelector('.movies');
+
 // RENDER MOVIES FROM OMDB API
 
 async function renderMovies(filter) {
    const moviesResponse = await fetch(`http://www.omdbapi.com/?apikey=b0099fba&s=${id}`);
    const moviesData = await moviesResponse.json();
    const moviesDataArray = moviesData.Search.slice(0,8)
+
+   moviesContainer.classList.remove('movies__loading')
 
    if (filter === "NEW_TO_OLD") {
     moviesDataArray.sort((a, b) => b.Year - a.Year);
@@ -14,7 +18,6 @@ async function renderMovies(filter) {
     moviesDataArray.sort((a, b) => a.Year - b.Year);
    }
 
-   const moviesContainer = document.querySelector('.movies');
    moviesContainer.innerHTML = moviesDataArray.map(movie => generateMovieHtml(movie)).join('');
 }
 
@@ -30,10 +33,12 @@ function filterMovies(event) {
 function generateMovieHtml(movie) {
     return `<div class="movie">
     <figure class="movie__img--wrapper">
-    <img src="${movie.Poster}" class="movie__img">
+        <img src="${movie.Poster}" class="movie__img">
     </figure>
-    <div class="movie__title">${movie.Title}</div>
-    <span class="movie--year">${movie.Year}</span>
+    <div class="movie__description">
+        <div class="movie__title">${movie.Title}</div>
+        <span class="movie--year primary--accent">${movie.Year}</span>
+    </div>
     </div>`;
 }
 
@@ -41,6 +46,21 @@ function generateMovieHtml(movie) {
 
 function searchInput(event) {
     id = event.target.value
-    renderMovies()
+    loadingState()
+    setTimeout(() => {
+        renderMovies()
+    }, 1000);
+}
+
+// LOADING STATE'
+function loadingState() {
+    const movieEl = document.querySelectorAll('.movie')
+    movieEl.forEach(element => element.remove())
+
+    moviesContainer.classList.add('movies__loading')
+    moviesContainer.innerHTML = `<i class="fas fa-spinner movies__loading--spinner"></i>`
+
+    const searchResult = document.querySelector('.search__results')
+    searchResult.innerHTML = `Search results for <span class="primary--accent">'${id}'</span>`
 }
 
